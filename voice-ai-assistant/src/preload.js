a7d4109816063c90558d6be6e97f65706f0454df
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-05-25 13:49:23 +0300

    delete: delete old files

--- voice-ai-assistant/src/preload.js
+++ voice-ai-assistant/src/preload.js
@@ -1,7 +1,2 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  startRecognition: () => ipcRenderer.invoke('start-recognition'),
-  stopRecognition: () => ipcRenderer.invoke('stop-recognition'),
-  onRecognitionResult: (callback) => ipcRenderer.on('recognition-result', (_, text) => callback(text)),
-});
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
